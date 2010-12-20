08ba6723467b2c309f1ab44670c99ac00e8a17c1
Mario Santagiuliana <mario@marionline.it>
2010-12-21 00:59:35 +0100

    Add options to script, erase colori script

--- ppt2pdf.sh
+++ ppt2pdf.sh
@@ -3,11 +3,11 @@
 #
 #          FILE:  ppt2pdf.sh
 # 
-#         USAGE:  ./ppt2pdf.sh 
+#         USAGE:  ./ppt2pdf.sh [options] [filename]
 # 
 #   DESCRIPTION:  
 # 
-#       OPTIONS:  ---
+#       OPTIONS: -r -v 
 #  REQUIREMENTS:  JODConverter, OpenOffice
 #          BUGS:  ---
 #         NOTES:  ---
@@ -18,82 +18,176 @@
 #	LICENSE: LGPL
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
+# Set JODConverter PATH
+JODConverter="/home/mario/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar"
 
-# .colori is my shell script with color code for bash
-. colori
+FILE=false
+RECURSIVELY=false
+VERBOSITY=1
+ASK=true
 
-echo -e "\n-----------------------------------------------------------------"
-echo -e "|   Avvio openoffice in modalità backgroud                      |"
-echo -e "|$RED soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard $Z          |"
-echo -e "-----------------------------------------------------------------\n"
+usage(){
+cat<<EOF
+
+Usage: ppt2pdf.sh [options] [filename]
+    options: -r: Recursive,
+    	     -y: Answare YES to all question,
+	     -v: Verbose (if not use default is 1),
+	     -h: Usage.
+    verbose: 0 no output,
+	     1 a little bit of info,
+	     2 a lot of info.
+
+EOF
+}
+
+file_size_diff(){
+    FILE_SIZE=$(ls -l $FILE | awk '{ print $5 }')
+    DEST_FILE_SIZE=$(ls -l $DEST_FILE | awk '{ print $5 }')
+
+    FILE_SIZE_HUMAN=$(ls -lh $FILE | awk '{ print $5 }')
+    DEST_FILE_SIZE_HUMAN=$(ls -lh $DEST_FILE | awk '{ print $5 }')
+
+    let DIFF="$FILE_SIZE - $DEST_FILE_SIZE"
+    if (($DIFF >= 1048576)); then
+	SCARTO=$(echo "scale=2; $DIFF/1048576" | bc)
+	UNIT="MByte"
+    else
+	SCARTO=$(echo "scale=2; $DIFF/1024" | bc)
+	UNIT="KByte"
+    fi
+}
+
+jod(){
+    java -jar $JODConverter $FILE $DEST_FILE
+}
+
+JOD_convert(){
+    if [[ $VERBOSITY =~ [1-2] ]]; then
+	echo $"Conversion of $FILE $(ls -lh $FILE | awk '{ print $5 }')..."
+    fi
+    if [[ $VERBOSITY =~ [0-1] ]]; then
+	jod &> /dev/null
+    else
+	jod
+    fi
+    if [[ $VERBOSITY =~ [1-2] ]]; then
+	echo -e $"Conversion finished.\n"
+	echo $"Original file $FILE is $(ls -lh $FILE | awk '{ print $5 }') size"
+	echo $"Converted file $DEST_FILE is $(ls -lh $DEST_FILE | awk '{ print $5 }') size"
+    fi
+}
+
+# If no parameter passed show usage options
+if [ $# -eq 0 ]
+then
+    echo $"No argument passed."
+    usage
+    exit 1
+fi
+
+# If not found JODConverter
+if [ ! -r "$JODConverter" ]; then
+  echo $"ERROR: JODConverter not found." >&2
+  exit 1
+fi
+
+while getopts ":hryv:" Options
+do
+    case $Options in 
+        h ) usage
+	    exit 0
+	    ;;
+        r ) RECURSIVELY=true;;
+	y ) ASK=false;;
+        v ) if [[ $OPTARG =~ [0-2] ]]
+	    then
+		VERBOSITY=$OPTARG
+            else
+		echo $"Invalid argument '$OPTARG' passed." >&2
+            fi
+            ;;
+        : ) echo $"No argument passed to '$OPTARG' option." >&2 
+	    exit 1
+	    ;;
+        * ) echo $"Invalid option '-$OPTARG'." >&2
+	    exit 1
+	    ;; # DEFAULT
+    esac
+done
+shift $(($OPTIND - 1))
+
+if [ -f $1 ]; then
+    FILE=$1
+fi
+PASSED_FILE=$FILE
+
+if [[ $VERBOSITY = 2 ]]; then
+    echo -e $"\nStarting OpenOffice as a deamon...\n"
+fi
 # start OpenOffice as a service on listen port 8100
 soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard &
 # save pid of started process
 pidOO=$!
-# Chiedo se convertire tutti i file ora o se passarli uno ad uno
-echo -n -e "$GREEN Convertire tutti i file ppt della directory corrente?$Z [s/...]\n"
-read d
-for file in *
-do
-     if echo "$file" | grep -q ".ppt" ; then
-       if [[ $d != 's' ]]; then
-         echo -e "Convertire:$RED $file$Z in pdf? [s/...]"
-         read -s -n1 n
-       else
-         n='s'
-       fi
-       if [[ $n = 's' ]] ; then
-          filePDF=${file/%.ppt/.pdf}
-	  if [ -f $filePDF ] ; then
-	    echo -e "   File $RED $filePDF $Z è già convertito: Size is $BLUE$(ls -lh $filePDF | awk '{ print $5 }')$Z"
-	    echo -e "   File $RED $file $Z di origine Size is $BLUE$(ls -lh $file | awk '{ print $5 }')$Z"
-	    # recupero le dimensioni dei file in modo da stampare sullo schermo le info sul loro peso.
-	    DimFile0=$(ls -l $file | awk '{ print $5 }')
-	    DimFile1=$(ls -l $filePDF | awk '{ print $5 }')
-	    let diff="$DimFile0 - $DimFile1"
-	    if (($diff >= 1048576)); then
-	       scarto=$(echo "scale=2; $diff/1048576" | bc)
-	       unita="MByte"
+# need a sleep to ensure that soffice complete the starting process
+sleep 2
+
+if [[  ${FILE: -4} == ".ppt" ]]; then
+    DEST_FILE=${FILE/%.ppt/.pdf}
+    JOD_convert
+fi
+
+if [[ $RECURSIVELY ]]; then
+    for FILE in *
+    do
+	 if [[  ${FILE: -4} == ".ppt" ]]; then
+	    DEST_FILE=${FILE/%.ppt/.pdf}
+	    CONTINUE=true
+	    if [[ -f $DEST_FILE ]] ; then
+		file_size_diff
+		if [[ $VERBOSITY =~ [1-2] ]]; then
+		    if [[ $FILE != $PASSED_FILE ]]; then
+			echo -e $"\nFile $DEST_FILE exist: Size is $DEST_FILE_SIZE_HUMAN"
+			echo $"Original file $FILE: Size is $FILE_SIZE_HUMAN"
+		    fi
+		fi
+		#echo $"Would you like overwrite it? [y/...]"
+		#read -s -n1 answare
+		#if [[ $answare = 'y' ]]; then
+		#fi
 	    else
-	       scarto=$(echo "scale=2; $diff/1024" | bc)
-	       unita="KByte"
+		if [[ $ASK ]]; then
+		    echo -e $"Convert: $FILE in pdf? [y/...]"
+		    read -s -n1 answare
+		    if [[ $answare = 'y' ]]; then
+			CONTINUE=true
+		    else
+			CONTINUE=false
+		    fi
+		fi
+		if [[ $CONTINUE ]] ; then
+		    JOD_convert
+		    file_size_diff
+		fi
 	    fi
-	    echo -e "   Risparmio di: $BLUE$scarto $unita $Z"
-	    echo
-	  else
-	    echo -e "$GRAY Inizio conversione di $file $(ls -lh $file | awk '{ print $5 }')"
-	    # Con questo comando indico a JODConverter di convertire i file
-	    # sto utilizzando la versione 2.2.1, il software è stato scaricato e scompattato nella home directory
-	    # dell'utente
-	    # modificare la path a jodconverter nel caso si utilizzi una versioni differente o posizione diversa del programma
-            java -jar ~/jodconverter-2.2.1/lib/jodconverter-cli-2.2.1.jar $file $filePDF
-	    echo -e "conversione finita:$Z"
-	    echo -e "   File originario$RED $file$Z di Size is $BLUE$(ls -lh $file | awk '{ print $5 }')$Z"
-	    echo -e "   Convertito in$RED $filePDF$Z di Size is $BLUE$(ls -lh $filePDF | awk '{ print $5 }')$Z"
-	    DimFile0=$(ls -l $file | awk '{ print $5 }')
-	    DimFile1=$(ls -l $filePDF | awk '{ print $5 }')
-	    let diff="$DimFile0 - $DimFile1"
-	    if (($diff >= 1048576)); then
-	       scarto=$(echo "scale=2; $diff/1048576" | bc)
-	       unita="MByte"
-	    else
-	       scarto=$(echo "scale=2; $diff/1024" | bc)
-	       unita="KByte"
+	    if [[ $VERBOSITY =~ [1-2] ]]; then
+		echo -e $"Saved: $SCARTO $UNIT \n"
 	    fi
-	    echo -e "   Risparmio di: $BLUE$scarto $unita $Z"
-	    echo
-          fi
-	  let dimtotPDF="$dimtotPDF + $DimFile1"
-	  let dimtotppt="$dimtotppt + $DimFile0"
-       fi
-     fi
-done
-# Informazioni su peso iniziale e finale di tutti i file convertiti
-dimtotppt=$(echo "scale=2; $dimtotppt/1048576" | bc)
-echo -e "Dimensione totale dei file ppt:$RED $dimtotppt$Z MB"
-dimtotPDF=$(echo "scale=2; $dimtotPDF/1048576" | bc)
-echo -e "Dimensione totale dei file pdf:$RED $dimtotPDF$Z MB"
-# Uccido il demone OpenOffice
-kill $pidOO
-exit
+	    let TOT_DEST_FILE_SIZE="$TOT_DEST_FILE_SIZE + $DEST_FILE_SIZE"
+	    let TOT_PPT_SIZE="$TOT_PPT_SIZE + $FILE_SIZE"
+	 fi
+    done
+    if [[ $VERBOSITY =~ [1-2] ]]; then
+	TOT_PPT_SIZE=$(echo "scale=2; $TOT_PPT_SIZE/1048576" | bc)
+	echo -e $"Total ppt files size: $TOT_PPT_SIZE MB"
+	TOT_DEST_FILE_SIZE=$(echo "scale=2; $TOT_DEST_FILE_SIZE/1048576" | bc)
+	echo -e $"Total pdf files size: $TOT_DEST_FILE_SIZE MB"
+    fi
+fi
+
+if [[ $VERBOSITY = 2 ]]; then
+    echo -e $"\nKill OpenOffice deamon...\n"
+fi
+# kill OpenOffice deamon
+kill $pidOO 2>/dev/null
+exit 0
